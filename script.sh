241d67dd9f0b4490893632bb8012f389a60f27c8
Your Name <my@email.org>
2025-01-30 13:22:54 +0000

    git commit

--- script.sh
+++ script.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-mkdir fol_1 fol_2
-touch "fol_1\1_1.txt"
-touch "fol_1\2_1.txt"
-touch "fol_1\3_1.txt"
-touch "fol_2\2_1.txt"
-touch "fol_2\2_2.txt"
-touch "fol_2\2_3.txt"
+rm -r fol_1
+rm -r fol_2
+mkdir fol_1 
+mkdir fol_2
+touch fol_1/{1_1.txt,1_2.txt,1_3.txt}
+touch fol_2/{2_1.txt,2_2.txt,2_3.txt}
